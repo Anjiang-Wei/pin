10f69a62c1497d933c8c40bc28f7c5e00fb02028
Anjiang-Wei <1020681930@qq.com>
2021-09-16 03:30:30 +0000

    try instrumenting load inside malloc, tbd

--- source/tools/ManualExamples/malloctrace.cpp
+++ source/tools/ManualExamples/malloctrace.cpp
@@ -18,6 +18,8 @@ using std::endl;
 using std::hex;
 using std::ios;
 using std::string;
+using std::vector;
+using std::pair;
 
 /* ===================================================================== */
 /* Names of malloc and free */
@@ -30,8 +32,10 @@ using std::string;
 // #define FREE "free"
 // #endif
 
-#define RRAM_MALLOC "rram_malloc"//"malloc"//"rram_malloc"
-#define RRAM_FREE "rram_free"//"free"//"rram_free"
+#define RRAM_MALLOC "rram_malloc"
+#define RRAM_FREE "rram_free"
+string RRAM_MALLOC_mangled = "";
+string RRAM_FREE_mangled = "";
 
 /* ===================================================================== */
 /* Global Variables */
@@ -43,33 +47,92 @@ std::ofstream TraceFile;
 /* Commandline Switches */
 /* ===================================================================== */
 
-KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "malloctrace.out", "specify trace file name");
+KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "1.out", "specify trace file name");
 
 /* ===================================================================== */
 
 /* ===================================================================== */
 /* Analysis routines                                                     */
 /* ===================================================================== */
+pair<ADDRINT, size_t> last_malloc_record;
+vector<pair<ADDRINT, size_t> > all_malloc_record;
 
-VOID Arg1Before(CHAR* name, ADDRINT size) { TraceFile << name << "(" << size << ")" << endl; }
+// Move from memory to register
+ADDRINT DoLoad(REG reg, ADDRINT* addr)
+{
+    TraceFile << "Emulate loading from addr " << addr << " to " << REG_StringShort(reg) << endl;
+    ADDRINT value;
+    PIN_SafeCopy(&value, addr, sizeof(ADDRINT));
+    return value;
+}
 
-VOID MallocAfter(ADDRINT ret) { TraceFile << "  returns " << ret << endl; }
+VOID Arg1Before(CHAR* name, ADDRINT size)
+{
+    TraceFile << name << "(" << size << ")" << endl;
+    last_malloc_record.first = size;
+}
+
+VOID MallocAfter(ADDRINT ret)
+{
+    TraceFile << "  returns " << ret << endl;
+    last_malloc_record.second = ret;
+    all_malloc_record.push_back(last_malloc_record);
+}
 
 /* ===================================================================== */
 /* Instrumentation routines                                              */
 /* ===================================================================== */
+bool isRRAM_addr(ADDRINT addr)
+{
+    return true;
+}
+
+VOID EmulateLoad(INS ins, VOID* v)
+{
+    // Find the instructions that move a value from memory to a register
+    if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryRead(ins) && INS_OperandIsReg(ins, 0) && INS_OperandIsMemory(ins, 1))
+    {
+        // op0 <- *op1
+        if (isRRAM_addr((ADDRINT) IARG_MEMORYREAD_EA)){
+            INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(DoLoad), IARG_UINT32, REG(INS_OperandReg(ins, 0)), IARG_MEMORYREAD_EA,
+                       IARG_RETURN_REGS, INS_OperandReg(ins, 0), IARG_END);
+
+            // Delete the instruction
+            INS_Delete(ins);
+        }
+    }
+}
+
+void find_mangled_symbols(IMG img)
+{
+    std::cout << "----------------------" << std::endl;
+    for (SYM sym = IMG_RegsymHead(img); SYM_Valid(sym); sym = SYM_Next(sym))
+    {
+        string s1 = SYM_Name(sym);
+        if (s1.find(RRAM_MALLOC) != std::string::npos)
+        {
+            RRAM_MALLOC_mangled = s1;
+        }
+        if (s1.find(RRAM_FREE) != std::string::npos && s1.find("GLOBAL") == std::string::npos)
+        {
+            RRAM_FREE_mangled = s1;
+        }
+    }
+    std::cout << RRAM_MALLOC_mangled << std::endl;
+    std::cout << RRAM_FREE_mangled << std::endl;
+}
 
 VOID Image(IMG img, VOID* v)
 {
+    find_mangled_symbols(img);
     // Instrument the malloc() and free() functions.  Print the input argument
     // of each malloc() or free(), and the return value of malloc().
     //
     //  Find the malloc() function.
-    RTN mallocRtn = RTN_FindByName(img, RRAM_MALLOC);
+    RTN mallocRtn = RTN_FindByName(img, RRAM_MALLOC_mangled.c_str());
     if (RTN_Valid(mallocRtn))
     {
         RTN_Open(mallocRtn);
-
         // Instrument RRAM_MALLOC() to print the input argument value and the return value.
         RTN_InsertCall(mallocRtn, IPOINT_BEFORE, (AFUNPTR)Arg1Before, IARG_ADDRINT, RRAM_MALLOC, IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
                        IARG_END);
@@ -79,7 +142,7 @@ VOID Image(IMG img, VOID* v)
     }
 
     // Find the free() function.
-    RTN freeRtn = RTN_FindByName(img, RRAM_FREE);
+    RTN freeRtn = RTN_FindByName(img, RRAM_FREE_mangled.c_str());
     if (RTN_Valid(freeRtn))
     {
         RTN_Open(freeRtn);
@@ -100,7 +163,7 @@ VOID Fini(INT32 code, VOID* v) { TraceFile.close(); }
 
 INT32 Usage()
 {
-    cerr << "This tool produces a trace of calls to RRAM_MALLOC." << endl;
+    cerr << "This tool produces a trace of calls to rram_malloc." << endl;
     cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
     return -1;
 }
@@ -125,6 +188,8 @@ int main(int argc, char* argv[])
 
     // Register Image to be called to instrument functions.
     IMG_AddInstrumentFunction(Image, 0);
+
+    INS_AddInstrumentFunction(EmulateLoad, 0);
     PIN_AddFiniFunction(Fini, 0);
 
     // Never returns
--- source/tools/ManualExamples/test.cpp
+++ source/tools/ManualExamples/test.cpp
@@ -16,7 +16,13 @@ void* rram_malloc(size_t size)
 
 int main()
 {
-    void* p = rram_malloc(16);
+    int* p = (int*) rram_malloc(0x10);
+    p[0] = 0x89;
+    // try more addresses on larger scale
     rram_free(p);
+
+    int* nopin = (int*) malloc(0x20);
+    nopin[0] = 0x98;
+    free(nopin);
     return 0;
 }
